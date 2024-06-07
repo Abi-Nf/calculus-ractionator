b7b01c06370b764b27d2fb4c58d345eaa792fc42
Abi-Nf <hei.abigail.3@gmail.com>
2024-06-07 16:22:13 +0300

    feat: Mensonge & Verité extend Affirmation

---
+++ src/main/java/calculus/ratiocinator/Mensonge.java
@@ -0,0 +1,7 @@
+package calculus.ratiocinator;
+
+public class Mensonge extends Affirmation {
+  public Mensonge(String phrase) {
+    super(phrase);
+  }
+}
---
+++ src/main/java/calculus/ratiocinator/Verité.java
@@ -0,0 +1,7 @@
+package calculus.ratiocinator;
+
+public class Verité extends Affirmation {
+  public Verité(String phrase) {
+    super(phrase);
+  }
+}
