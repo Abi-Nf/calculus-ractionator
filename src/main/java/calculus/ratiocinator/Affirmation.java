48dea518dc2cc7339f0dabdf109a5ff25fefdc8d
Abi-Nf <hei.abigail.3@gmail.com>
2024-06-07 16:19:34 +0300

    feat: Affirmation

---
+++ src/main/java/calculus/ratiocinator/Affirmation.java
@@ -0,0 +1,13 @@
+package calculus.ratiocinator;
+
+public class Affirmation {
+  private final String phrase;
+
+  public Affirmation(String phrase){
+    this.phrase = phrase;
+  }
+
+  public String phrase() {
+    return phrase;
+  }
+}
